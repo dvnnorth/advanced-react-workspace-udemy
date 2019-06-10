845a4ea042909e1acf0effee2fcffcef1a832d14
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2019-06-09 23:34:58 -0400

    Handled conflict with test.js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 let x = 0;
 let adder = n => x => x + 10;
 let add10 = adder(10);
+let add20 = adder(20);
+
 console.log("x: " + x);
 console.log("add10(x): " + add10(x));
 console.log("add10(add10(x)): " + add10(add110(x)));
-
-
+console.log("add20(x): " + add20(x));
+console.log("add20(add10(x)): " + add20(add10(x)));
